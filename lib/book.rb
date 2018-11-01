df5b1540fd840c0ac448f438fe5b02c05191c836
Jacob Feldman <jacobgfeld@gmail.com>
2018-11-01 15:48:25 +0000

    automatically backed up by learn

--- lib/book.rb
+++ lib/book.rb
@@ -1,9 +1,11 @@
 class Book
+  attr_reader :title 
+  attr_accessor :author, :page_count, :genre
+  
   def initialize(title)
     @title = title
   end
-  attr_reader :title 
-  attr_accessor :author, :page_count, :genre
+
   
   def turn_page
     puts "Flipping the page...wow, you read fast!"
